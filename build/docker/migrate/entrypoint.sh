6097caca0e5e95f54ab1fbc2ce6afd9b7c7acc1f
Zan Baldwin <hello@zanbaldwin.com>
2022-01-27 23:04:44 +0100

    Update Migrations Entrypoint Script
    - Pull database password from runtime secret if exists.
    - Add default number of max attempts if invalid number supplied.

--- build/docker/migrate/entrypoint.sh
+++ build/docker/migrate/entrypoint.sh
@@ -4,44 +4,54 @@
 # @param $2 default fallback value.
 function run_secret {
     if [ -f "/run/secrets/$1" ]; then
-        echo "$(cat "/run/secrets/$1" | tr -d '[:space:]')"
+        tr -d '[:space:]' < "/run/secrets/$1"
+        return 0
     fi
-    echo "$2"
+    if [ -z "$2" ]; then
+        echo "$2"
+    fi
+    return 1
 }
 
 # @param $1 max number of retry attempts.
 # @params   command to make repeated attempts to execute successfully.
 function retry {
-    local MAX_ATTEMPTS=$1
+    local MAX_ATTEMPTS
+
+    if [[ "${1}" =~ ^[0-9]+$ ]] && (( $1 > 0)); then
+        MAX_ATTEMPTS=$1
+    else
+        MAX_ATTEMPTS=3
+        echo >&2 "Defaulting to ${MAX_ATTEMPTS} attempts."
+    fi
     shift
 
     local COUNT=${MAX_ATTEMPTS}
     while [ ${COUNT} -gt 0 ]; do
-        eval "$*" && break
+        eval "$*" && return 0
+        echo >&2 "Command failed; ${COUNT} attempt(s) remaining."
         COUNT=$((${COUNT} - 1))
         sleep 5
     done
 
-    [ ${COUNT} -eq 0 ] && {
-        echo "Retry failed [${MAX_ATTEMPTS}]: $*" >&2
-        exit 1;
-    }
-    return 0
+    echo >&2 "Retry failed [${MAX_ATTEMPTS}]: $*"
+    exit 1;
 }
 
 function migrate {
     eval $(dbenv "$1") || { echo "Could not parse database information from database DSN." >&2; exit 2; }
     # If a runtime secret has been passed, use that instead of what was in the DSN.
-    export DB_PASS="$(run_secret "${PASSWORD_SECRET_NAME}" "${DB_PASS}")"
+    DB_PASS="$(run_secret "${PASSWORD_SECRET_NAME}" "${DB_PASS}")"
+    export DB_PASS
     # Retry every 5 seconds for a maximum of 18 times (90 seconds total). If MySQL
     # isn't up by then you should probably investigate.
-    retry 18 mysql \
-            --user=\"${DB_USER}\" \
-            --password=\"${DB_PASS}\" \
-            --host=\"${DB_HOST}\" \
-            --port=\"${DB_PORT}\" \
-            --database=\"${DB_NAME}\" \
-            --execute=\"SELECT 1\"\
+    retry 1 mysql \
+            "--user=\"${DB_USER}\"" \
+            "--password=\"${DB_PASS}\"" \
+            "--host=\"${DB_HOST}\"" \
+            "--port=\"${DB_PORT}\"" \
+            "--database=\"${DB_NAME}\"" \
+            "--execute=\"SELECT 1\"" \
             >/dev/null \
         && echo "Successfully connected to database \"${DB_NAME}\" on host \"${DB_HOST}\"; proceeding to perform migrations." \
         && /sbin/migrate \
@@ -56,5 +66,3 @@ if [ -z "${DATABASE_URL}" ]; then
 fi
 
 migrate "${DATABASE_URL}"
-
-exit 0
